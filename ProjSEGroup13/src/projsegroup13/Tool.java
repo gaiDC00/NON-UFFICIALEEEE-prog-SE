3067e1969e1a7580883e7cfb749e4c83519a2907
nicco0197 <niccogallons01@gmail.com>
2022-11-25 21:50:44 +0100

    prova commit

---
+++ ProjSEGroup13/src/projsegroup13/Tool.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package projsegroup13;
+
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author 109017
+ */
+public interface Tool {
+    
+    public void action(MouseEvent e, Pane pane);
+    
+}
