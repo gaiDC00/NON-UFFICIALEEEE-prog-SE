69186dfe252cf784cb1bff594512a8d1bee71ca7
gaiDC00 <g.dicarlo2@studenti.unisa.it>
2022-11-25 20:53:28 +0100

    modified:   ProjSEGroup13/src/projsegroup13/FXMLDocumentController.java

--- ProjSEGroup13/src/projsegroup13/FXMLDocumentController.java
+++ ProjSEGroup13/src/projsegroup13/FXMLDocumentController.java
@@ -23,7 +23,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("Commit con VS CODE!");
         label.setText("Hello World!");
     }
     
